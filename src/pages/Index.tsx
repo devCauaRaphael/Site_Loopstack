e4da886a8037f0655d49f8598cbb001a16cdb37c
Cauã <dev.cauaraphael@gmail.com>
2025-08-10 10:49:02 -0300

    desenvolvido as páginas

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import AppSidebar from "@/components/AppSidebar";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Team from "@/components/Team";
+import Clients from "@/components/Clients";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <AppSidebar />
+      <main>
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Team />
+        <Clients />
+        <Contact />
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default Index;
